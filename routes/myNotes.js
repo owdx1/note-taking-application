99abe5f946232d4ac50df175a3eac1192c8f490a
Can Bozkurt <can.bozkurt.dhf@gmail.com>
2023-07-09 00:25:44 +0300

    route added

--- routes/myNotes.js
+++ routes/myNotes.js
@@ -9,15 +9,34 @@ router.get('/' , accessTokenValidator , async (req , res) =>{
 
     try {
         
-        const notes = await pool.query('SELECT * FROM notes WHERE user_id = $1' , [user.id]);
+        const userQuery = await pool.query('SELECT * FROM notes WHERE user_id = $1' , [user.id]);
 
-        return res.status(200).json(notes);
+        userNotes = [];
+
+        for (let i = 0; i < userQuery.rows.length; i++){
+            userNotes.push(userQuery.rows[i].user_note)
+        }
+
+        return res.status(200).json(userNotes);
         
     } catch (error) {
         console.error(error);
         return res.status(500).send('Server error');
     }
     
+})
+router.post('/add-new-note' , accessTokenValidator , async (req , res) =>{
+    const {user} = req;
+    const {note} = req.body;
+    try {
+        await pool.query("INSERT INTO notes (user_id , user_note) values ($1 , $2)",  [user.id , note])
+
+        return res.status(200).send('Note added successfully: ');
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error');
+    }
 })
 
 module.exports = router
