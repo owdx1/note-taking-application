00b97372354758ec41aba7a33c6f9493cfb35fac
Can Bozkurt <can.bozkurt.dhf@gmail.com>
2023-07-12 18:29:19 +0300

    shop routuna ekleme yapıldı

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,36 @@
 const shopRouter = require('express').Router();
+const { raw } = require('mysql');
+const pool = require('../db');
 
 
-shopRouter.get('/' , (req , res) =>{
-    res.status(200).send('hi');
+shopRouter.get('/' , async (req , res) => {
+
+    try {
+
+        const rawData = await pool.query('SELECT * FROM products where quantity > 0');
+        const data = rawData.rows;
+
+        return res.status(200).json({data})
+
+
+    } catch (error) {
+        console.error(error);
+        
+        return res.status(500).json({message: "An error occured while fetching the products where quantity > 0"})
+        
+    }
+
+    
+
+    
+
+
+
+
+    
+
+
+
 })
 
 module.exports = shopRouter;
