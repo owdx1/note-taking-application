5b833bfeb3cd174ad2e76d5f760926809480092b
vural <vuraldesu@gmail.com>
2023-07-14 16:48:37 +0300

    yeni DBye göre

--- routes/admin.js
+++ routes/admin.js
@@ -34,19 +34,46 @@ adminRouter.post('/add-a-product' , adminTokenValidator , async (req , res) => {
         const { product_name,
             category_id,
             price,
-            quantity,
             color,
-            size,
             pattern,
-            description 
+            description ,
+            size,
         } = req.body;
-        const product_size = size;
+
+
+        // feature_id
+        //XS -> 1
+        //S->2
+        //M -> 3
+        // L->4
+        // XL ->5
+        // XXL ->6
+        
+       /* const avilableProduct=await pool.query("SELECT * from products where product_id=$1 ",[product_id]);
         const newQuery = (category_id == 2) ? 
-            await pool.query("INSERT INTO products (product_name, category_id, price,quantity, color, size_i, pattern, description) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
-                [product_name,category_id,price,quantity ,color ,product_size ,pattern ,description])
+            await pool.query("INSERT INTO products (product_name, category_id, price, color, pattern, description) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+                [product_name,category_id,price ,color  ,pattern ,description])
                 :
-            await pool.query("INSERT INTO products (product_name, category_id, price, quantity, color, size, pattern, description) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
-                [product_name,category_id,price,quantity ,color ,product_size ,pattern ,description]);
+            await pool.query("INSERT INTO products (product_name, category_id, price, color, pattern, description) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+                [product_name,category_id,price ,color  ,pattern ,description]);
+            */  
+                if(category_id==2){
+                    await pool.query("INSERT INTO products (product_name, category_id, price, color, pattern, description) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+                [product_name,category_id,price ,color  ,pattern ,description]);
+                    // ürün varsa diye kontrol etmeli burda
+                    
+                    await pool.query("INSERT INTO feature(product_id,size_i,quantity) values($1,$2,$3)",[product_id,size,quantity]);
+                }
+                else{
+                    await pool.query("INSERT INTO products (product_name, category_id, price, color, pattern, description) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+                [product_name,category_id,price ,color  ,pattern ,description]);
+                    // ürün varsa diye kontrol etmeli
+
+
+                    await pool.query("INSERT INTO feature(product_id,size,quantity) values($1,$2,$3)",[product_id,size,quantity]);
+                }
+
+
 
         return res.status(200).json({message: 'New product added successfully!'})
 
@@ -59,7 +86,7 @@ adminRouter.post('/add-a-product' , adminTokenValidator , async (req , res) => {
 adminRouter.get('/dashboard' , adminTokenValidator , async (req , res) => {
     const {admin} = req;
     try {
-        const productsAll = await pool.query('SELECT * FROM products');
+        const productsAll = await pool.query('SELECT * FROM products');//anasayfada yayınlanan ürünler 
         // bu dashboard'da toplam satılan ürün sayısı, kazanılan toplam miktar, ve ürünlerin bulunduğu bir sekme yer alacak.
         const products  = productsAll.rows;
 
@@ -119,24 +146,53 @@ adminRouter.post('/patch-a-product/:product_id' , adminTokenValidator , async (r
         description 
     } = req.body;
 
-    const product = await pool.query('SELECT * FROM customers where product_id = $1' , [product_id]);
+    const product = await pool.query('SELECT * FROM products where product_id = $1' , [product_id]);
 
-    if(note.rows.length === 0){
+    if(product.rows.length === 0){
         return res.status(404).send('Note not found')
     }
 
-    await pool.query('UPDATE notes SET product_name = $1, category_id = $2, price = $3, quantity = $4, color = $5, size = $6, pattern = $7, description = $8 WHERE product_id = $9' , 
-    [ product_name, category_id, price, quantity, color, size, pattern, description, product_id]);
+    await pool.query('UPDATE products SET product_name = $1, category_id = $2, price = $3, color = $4, pattern = $5, description = $6 WHERE product_id = $7' , 
+    [ product_name, category_id, price, color, pattern, description, product_id]);
 
     return res.status(200).json({message: 'Product updated successfully'});
 });
 
-adminRouter.get('/patch-a-product/:product_id' , adminTokenValidator , async (req , res) => {
+adminRouter.get('/patch-a-product/:product_id/:feature_id' , adminTokenValidator , async (req , res) => {
     const {product_id} = req.params;
-    console.log(product_id);
+    const { size,
+        quantity
+    } = req.body;
 
-}) // buna gerek var mı bakacaz
 
 
+}) 
+
+adminRouter.put('/product-feature/:product_id/:feature_id',adminTokenValidator,async(res,req)=>{
+    try {
+        const{quantity}=req.body;
+        const query=await pool.query("UPDATE feature SET quantity=$1 WHERE product_id=$2 AND feature_id=$3",[quantity,product_id,feature_id]);
+
+        return res.status(200).json({message: 'INNER product updated successfully'});
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error');
+    }
+})
+
+
+adminRouter.put('/product-feature/:product_id',adminTokenValidator,async(res,req)=>{
+    try {
+        const{product_name,price,discount,description}=req.body;
+        const query=await pool.query("UPDATE product SET product_name=$1,price=$2,discount=$3,description=$4 WHERE product_id=$2 ",[product_name,price,discount,description,product_id]);
+
+        return res.status(200).json({message: 'Product updated successfully'});
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server error');
+    }
+})
 
 module.exports = adminRouter;
--- routes/shop.js
+++ routes/shop.js
@@ -11,13 +11,13 @@ const { raw } = require('mysql');
 const pool = require('../db');
 
 
-<<<<<<< HEAD
+
 const pool = require('../db');
 const accessTokenValidator = require('../middlewares/accessTokenValidator');
 const refreshTokenValidator = require('../middlewares/refreshTokenValidator');
 
 
-shopRouter.post('/add-product',accessTokenValidator,refreshTokenValidator,async(req,res)=>{
+shopRouter.post('/add-basket',accessTokenValidator,refreshTokenValidator,async(req,res)=>{
     try {
 
         const {customer}=req;
@@ -27,18 +27,19 @@ shopRouter.post('/add-product',accessTokenValidator,refreshTokenValidator,async(
         if(query.rows.length===0){
           const newQuery=await pool.query("INSERT INTO orders(customer_id,total_amount) VALUES($1,$2)",[id,0]);
         }
-        else{
+        
         
 
           const newestOrder = await pool.query('SELECT * FROM orders ORDER BY order_date DESC WHERE customer_id=$1' , [customer.id]);
-          const newOrderId = newestOrder.rows[0].order_id;//en son siparişi listeler
+          const newOrderId = newestOrder.rows[0].order_id;//en son siparişin idsi
 
 
 
           const priceResult=await pool.query("SELECT price from products where product_id=$1",[product_id]);
           const price = parseFloat(priceResult.rows[0].price);// eklencek ürünün fiyatı
 
-          const avilableProduct=await pool.query("SELECT * from order_items where product_id=$1 AND order_id=$2",[product_id,newOrderId]);
+          const avilableProduct=await pool.query("SELECT * from order_items I, products P,feature F where product_id=$1 AND order_id=$2 AND I.product_id=P.product_id AND P.product_id=F.product_id  ",[product_id,newOrderId]);
+          //!!!!!!!!!!!!!!!!!!!!
           if(avilableProduct.rows.length===0){//eğer daha önce  sepette yoksa ekle , varsa üzerine ekle
             const newQuery=await pool.query("INSERT INTO order_items(order_id,product_id,quantity,price) values($1,$2,$3,$4)",[newOrderId,product_id,quantity,price*quantity]);
           }
@@ -53,7 +54,7 @@ shopRouter.post('/add-product',accessTokenValidator,refreshTokenValidator,async(
             newPrice = newQuantity*price;
              const updateQuery=await pool.query("UPDATE order_items SET quantity=$1,price=$2 where product_id=$3 and order_id=$4",[newQuantity,newPrice,product_id,newOrderId]);
           }
-        }
+        
         const idQuery=await pool.query("SELECT * FROM order_items ");
         res.json(idQuery.rows);
 
@@ -72,7 +73,7 @@ shopRouter.delete('/delete-product/:product_id',accessTokenValidator,refreshToke
         const newestOrder = await pool.query('SELECT * FROM orders ORDER BY order_date DESC WHERE customer_id=$1' , [customer.id]);
           const newOrderId = newestOrder.rows[0].order_id;//en son siparişi listeler
 
-        const avilableProduct=await pool.query("SELECT * FROM order_items WHERE order_id=$1 and product_id=$2",[newOrderId,product_id])
+        const avilableProduct=await pool.query("SELECT * from order_items I, products P,feature F where product_id=$1 AND order_id=$2 AND I.product_id=P.product_id AND P.product_id=F.product_id  ",[product_id,newOrderId]);
         if(avilableProduct.rows.length===0){        return res.status(404).send('Note not found');    }
         else{
             await pool.query("DELETE FROM order_items WHERE order_id=$1 AND product_id=$2",[newOrderId,product_id]);
@@ -93,8 +94,8 @@ shopRouter.put('/update-quantity/:product_id',accessTokenValidator,refreshTokenV
         const newestOrder = await pool.query('SELECT * FROM orders ORDER BY order_date DESC WHERE customer_id=$1' , [customer.id]);
           const newOrderId = newestOrder.rows[0].order_id;//en son siparişi listeler
 
-        const avilableProduct=await pool.query("SELECT * FROM order_items WHERE order_id=$1 and product_id=$2",[newOrderId,product_id]);
-        if(avilableProduct.rows.length===0){        return res.status(404).send('Note not found');    }
+          const avilableProduct=await pool.query("SELECT * from order_items I, products P,feature F where product_id=$1 AND order_id=$2 AND I.product_id=P.product_id AND P.product_id=F.product_id  ",[product_id,newOrderId]);
+          if(avilableProduct.rows.length===0){        return res.status(404).send('Note not found');    }
         else{
             const oldQuantityResult=await pool.query("SELECT quantity FROM order_items WHERE order_id=$1 and product_id=$2",[newOrderId,product_id]);
             const oldQuantity=parseInt(oldQuantityResult.rows[0].quantity);
@@ -103,7 +104,6 @@ shopRouter.put('/update-quantity/:product_id',accessTokenValidator,refreshTokenV
             const newPrice = newQuantity*price;
             await pool.query("UPDATE order_items SET  quantity=$3,price=$4 WHERE order_id=$1 AND product_id=$2",[newOrderId,product_id,newQuantity,newPrice]);
             return res.status(200).json({message: 'Product updated successfully'});
-
         }
 
     } catch (error) {
@@ -113,15 +113,13 @@ shopRouter.put('/update-quantity/:product_id',accessTokenValidator,refreshTokenV
 });
 
 
-shopRouter.post('/' ,accessTokenValidator,refreshTokenValidator, (req , res) =>{
-    
-    res.status(200).send('hi');
-=======
+
+
 shopRouter.get('/' , async (req , res) => {
 
     try {
 
-        const rawData = await pool.query('SELECT * FROM products');
+        const rawData = await pool.query('SELECT * FROM products');// ürünün genel görüntüsü
         const data = rawData.rows;
 
         return res.status(200).json({data})
@@ -134,7 +132,20 @@ shopRouter.get('/' , async (req , res) => {
         
     }
 
->>>>>>> 40e17eae0865cf2a869ec721c7f1e00188c469d0
+
+});
+shopRouter.get('/:product_id',async(req,res)=>{//ürünün üzerine tıklayınca gelen ürün dataları
+    try {
+        const{product_id}=req.params;
+        const rawData = await pool.query('SELECT * FROM products P,feature F WHERE product_id=$1 AND F.product_id=P.product_id',[product_id]);
+        const data = rawData.rows;
+
+        return res.status(200).json({data})
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Server Error');
+    }
 })
 
 module.exports = shopRouter;
