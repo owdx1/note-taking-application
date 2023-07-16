d03d41fdc903c50684f7180cc0f8e0ea32cb34a6
vural <vuraldesu@gmail.com>
2023-07-16 21:55:34 +0300

    available size yapildi

--- routes/shop.js
+++ routes/shop.js
@@ -12,18 +12,56 @@ const pool = require('../db');
 const accessTokenValidator = require('../middlewares/accessTokenValidator');
 const refreshTokenValidator = require('../middlewares/refreshTokenValidator');
 
+
+async function getBasketItemCount(customerId) {
+
+    
+  
+    try {
+
+      const newestOrder = await pool.query('SELECT * FROM orders  WHERE customer_id=$1 ORDER BY order_date DESC' , [customerId]);
+    const newOrderId = newestOrder.rows[0].order_id;//en son siparişin idsi
+
+    const productNumResult = await pool.query("SELECT SUM(quantity) FROM order_items WHERE order_id = $1", [newOrderId]);
+    let productNum=productNumResult.rows[0].sum;
+    
+      
+      return productNum
+    } catch (error) {
+      console.error('Error retrieving basket item count:', error);
+      throw error;
+    }
+  }
+
+
+
+
+
+
 shopRouter.get('/basket',accessTokenValidator,refreshTokenValidator,async(req,res)=>{
     try {
         const {customer}=req;
         const{id}=customer;
         const newestOrder = await pool.query('SELECT * FROM orders  WHERE customer_id=$1 ORDER BY order_date DESC' , [customer.id]);
         const newOrderId = newestOrder.rows[0].order_id;//en son siparişin idsi
-        
+        const categoryID=await pool.query('')
         const cart=await pool.query("SELECT P.product_name,p.color,o.price,F.size_i,F.size,F.quantity from order_items o,products p ,feature F WHERE order_id=$1 and o.product_id=p.product_id and p.product_id=F.product_id",[newOrderId]);
-
+        
         const data=cart.rows;
         
-        return res.status(200).json({data});
+      // const newData = data.map(({ quantity, ...rest }) => rest);
+
+    
+                
+                const newData = data.map(item => {
+                    if (item.size === null) {
+                        delete item.size;
+                    }
+                    if(item.size_i===null){delete item.size_i;}
+                    return item;
+                });
+
+        return res.status(200).json({newData});
         
     } catch (error) {
         console.error(error);
@@ -67,8 +105,7 @@ shopRouter.post('/add-basket',accessTokenValidator,refreshTokenValidator,async(r
             newPrice = newQuantity*price;
              const updateQuery=await pool.query("UPDATE order_items SET quantity=$1,price=$2 where product_id=$3 and order_id=$4",[newQuantity,newPrice,product_id,newOrderId]);
           }
-          const productNumResult = await pool.query("SELECT SUM(quantity) FROM order_items WHERE order_id = $1", [newOrderId]);
-          let productNum=productNumResult.rows[0].sum;
+          const productNum = await getBasketItemCount(customer.id);
            console.log("real:",productNum);
           
           return res.status(200).json({message: 'Added in basket  successfully' , productNum: productNum});
@@ -141,6 +178,9 @@ shopRouter.get('/' , async (req , res) => {
         const data = rawData.rows;
         
 
+      /*  const productNum = await getBasketItemCount(customer.id);
+           console.log("real:",productNum);
+*/
         return res.status(200).json({data})
 
 
@@ -158,20 +198,24 @@ shopRouter.get('/:product_id',async(req,res)=>{//ürünün üzerine tıklayınca
         const{product_id}=req.params;
         const rawData = await pool.query('SELECT * FROM products P,feature F WHERE P.product_id=$1 AND F.product_id=P.product_id',[product_id]);
         let data=rawData.rows;
+        const productQuantity=data.quantity;
         if(rawData.rows[0].category_id===6){//available sizelari döndürüyor
             const sizeIsNotNull = data
             .filter(item => item.size_i !== null)
                 .map(item => item.size_i);
+            
+
+                const transformedData = data.map(({ size_i, quantity }) => ({ size_i, quantity }));
 
             console.log(sizeIsNotNull);
-            return res.status(200).json({sizeIsNotNull})
+            return res.status(200).json({transformedData,sizeIsNotNull})
         }else{
             const sizeIsNotNull = data
             .filter(item => item.size !== null)
                 .map(item => item.size);
-
+                const transformedData = data.map(({ size, quantity }) => ({ size, quantity }));
             console.log(sizeIsNotNull);
-            return res.status(200).json({sizeIsNotNull})
+            return res.status(200).json({transformedData,sizeIsNotNull})
         }
 
         
